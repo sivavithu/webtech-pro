a8d481c69de87687dc86399891d9d10f789fa5ba
sivavithu <sivavithu15@live.com>
2023-08-21 23:20:03 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,6 @@
 <?php
-$hostname="jbdc:mysql://sql6.freesqldatabase.com:3306/sql6641386
-";
-$username="sql6641386
-";
+$hostname="jbdc:mysql://sql6.freesqldatabase.com:3306/sql6641386";
+$username="sql6641386";
 $password="eyrTh6Z6Ql";
 $dbname="sql6641386";
 $con=mysqli_connect($hostname,$username,$password,$dbname);
