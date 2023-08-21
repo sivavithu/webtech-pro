787f7dfff7031401e50839b3010f951f5ff3e995
sivavithu <sivavithu15@live.com>
2023-08-21 22:26:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user_id'])&& isset($_SESSION['role'])){
+        if($_SESSION['role']=='admin'){
+            header("location:adminuser.php");
+            exit;}
+    
+    else {
+        header("location:student/home.php");
+        exit;
+    }
+}?>
