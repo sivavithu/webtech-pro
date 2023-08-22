09e6eac56d38d894c3ef38c39c72d53a8483ed9c
sivavithu <sivavithu15@live.com>
2023-08-22 10:29:45 +0530

    Add files via upload

---
+++ adminuser.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     session_start();
+      echo $_SESSION['role'];
+
+     if(isset($_SESSION['user_id'])&& isset($_SESSION['role'])&& $_SESSION['role']=='admin'){
+       
+?>
+
+<h1>hello admin</h1>
+    <a href="logout.php"><button>logout</button></a>
+    
+    <?php }
+    else{
+        header("location:Login.php");
+        exit;
+    }
+    
+    ?>
+    
+</body>
+</html>
