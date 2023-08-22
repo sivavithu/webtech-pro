ddcb689b7bc172b4aded09e5e1dd97a725016a16
sivavithu <sivavithu15@live.com>
2023-08-22 10:28:12 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $hostname="db4free.net";
 $username="webtechpro1";
 $password="king1234";
-$dbname="sql6641386";
+$dbname="webtechpro1";
 $con=mysqli_connect($hostname,$username,$password,$dbname);
 if(!$con)
 {
